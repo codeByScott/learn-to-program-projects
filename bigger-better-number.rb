fd8643813b014e197528a156853631452f462381
codeByScott <scott@codebyscott.io>
2016-02-22 14:11:01 +0900

    Completed bigger-better-number.rb

---
+++ bigger-better-number.rb
@@ -0,0 +1,7 @@
+# Bigger, Better, Number
+
+puts "What is your favorite number?"
+fave_num = gets.chomp.to_i
+better_num = fave_num + 1
+
+puts "Nice, #{fave_num} is a great number. I would suggest considering #{better_num}. The number #{better_num} is a bigger, better number."
