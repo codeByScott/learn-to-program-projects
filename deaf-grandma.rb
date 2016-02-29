e0f3b6a9393502f2539dbfa17b60810706a4a7a7
codeByScott <scott@codebyscott.io>
2016-02-29 10:11:01 +0900

    exercise complete

---
+++ deaf-grandma.rb
@@ -0,0 +1,38 @@
+# Deaf grandma. Whatever you say to Grandma 
+# (whatever you type in), 
+# she should respond with this:
+# ￼￼￼HUH?! SPEAK UP, SONNY!
+# unless you shout it (type in all capitals). 
+# If you shout, she can hear you (or at least she thinks so) 
+# and yells back: NO, NOT SINCE 1938
+# To make your program really believable, have Grandma shout a different year each time, 
+# maybe any year at random between 1930 and 1950. 
+# (This part is optional and would be much easier 
+# if you read the section on Ruby’s random number generator on page 38.) 
+#You can’t stop talking to Grandma until you shout BYE.
+# Hint 1: Don’t forget about chomp! 'BYE' with an Enter at the end is not 
+# the same as 'BYE' without one!
+# Hint 2: Try to think about what parts of your program should 
+# happen over and over again. All of those should be in your while loop.
+# Hint 3: People often ask me, “How can I make rand give me a number in a range 
+# not starting at zero?” Well, you can’t; rand just doesn’t work that way. 
+# So, I guess you’ll have to do something to the number rand returns to you.
+
+
+puts "Say Something to Grandma."
+phrase = gets.chomp
+counter = 0
+
+while phrase != 'BYE' do
+  
+  if phrase != phrase.upcase
+    puts "HUH?! SPEAK UP, SONNY!"
+    counter += 1
+  else
+    puts "NO, NOT SINCE #{1935 + rand(50)}" 
+    # I know I changed the criteria here, but my Grandma was born 1935, and thought it would be more fun for her to randomly pick a year between 35 and 85
+    counter += 1
+  end
+  puts "Say(type) 'BYE' if you give up, or type in all caps!" if counter == 3
+  phrase = gets.chomp
+end
