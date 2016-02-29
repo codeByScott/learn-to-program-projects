78398e4077804af6675818bfd4978d2f7c463e2c
codeByScott <scott@codebyscott.io>
2016-02-29 12:25:36 +0900

    initiated file

---
+++ improved-ask.rb
@@ -0,0 +1,9 @@
+# That ask method I showed you was OK, but I bet you could do better. 
+# Try to clean it up by removing the answer variable. 
+#You’ll have to use return to exit from the loop. 
+#(Well, it will get you out of the whole method, 
+# but it will get you out of the loop in the process.) 
+# How do you like the resulting method? 
+# I usually try to avoid using return (a personal preference), 
+# but I might make an exception here.
+
